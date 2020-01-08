02dd96aa447a4a01f3921921123dd3902687ec34
prazd <alexokda@list.ru>
2020-01-08 23:47:36 +0300

    update new-account

--- wallet-frontend/src/app/account/new/new-account.component.ts
+++ wallet-frontend/src/app/account/new/new-account.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { HdWallet } from '../../shared/services/hd-wallet/hd-wallet.service';
+import {StorageService} from '../../shared/services/storage/storage.service';
+import {Security} from '../../shared/services/security/security.service';
+import {Options, QrCode} from '../../shared/components/qrcode/qrcode.service';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -8,6 +12,12 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
+}
+
+interface IRow {
+  label: string;
+  address: string;
+  privateKey: string;
 }
 
 /** @title Input with a custom ErrorStateMatcher */
@@ -37,14 +47,18 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     `
   ],
 })
+
+
 export class NewAccountComponent {
-
-  color = 'accent';
+  data: Array<IRow> = [];
+  words: string;
+  password = '';
   checked = false;
-
-  get email() { return this.newAccountForm.get('email'); }
-
-  // get power() { return this.heroForm.get('power'); }
+  display = true;
+  href: string;
+  formData: FormData;
+  @ViewChild('qrcode', { static: false }) qrcode: ElementRef;
+  color = 'accent';
 
   newAccountForm = new FormGroup({
     email: new FormControl('', [
@@ -59,4 +73,60 @@ export class NewAccountComponent {
   });
 
   matcher = new MyErrorStateMatcher();
+
+  get email() { return this.newAccountForm.get('email'); }
+
+  generateMnemonic() {
+    const newMnemonic = HdWallet.generateMnemonic();
+
+    const s = new StorageService();
+
+    document.querySelector('div#qrcode').innerHTML = '';
+
+    const cypher = Security.encryptSecret(newMnemonic, this.password);
+    s.cypherParams = { salt: cypher.salt, iv: cypher.iv };
+    s.storage = { secret: cypher.text, expired: false };
+
+    const opt: Options = { text: cypher.text };
+    const qr = new QrCode();
+    qr.render(opt, this.qrcode);
+
+    this.words = newMnemonic;
+    const mnemonic = Security.decryptSecret(cypher.text, this.password, cypher.salt,  cypher.iv);
+    console.log(mnemonic);
+  }
+
+  generateKeyPairs() {
+    const hdWallet = new HdWallet(this.words, this.password);
+    const { btc, ltc, eth, bch, etc, waves, xlm } = hdWallet.generateAllKeyPairs(0);
+    this.data = [
+      { label: 'BTC', address: btc.address, privateKey: btc.privateKey },
+      { label: 'LTC', address: ltc.address, privateKey: ltc.privateKey },
+      { label: 'BCH', address: bch.address, privateKey: bch.privateKey },
+      { label: 'ETH', address: eth.address, privateKey: eth.privateKey },
+      { label: 'ETC', address: etc.address, privateKey: etc.privateKey },
+      { label: 'XLM', address: xlm.address, privateKey: xlm.privateKey },
+      { label: 'Waves', address: waves.address, privateKey: waves.privateKey },
+    ];
+  }
+
+  // private sendQrCodeToEmail(qrCode, shortlink) {
+  //   this.formData = new FormData();
+  //   this.formData.append('file', , 'BUTTONWallet.jpg');
+  //   return fetch(`.../${shortlink}`, {method: 'POST', body: this.formData});
+  // }
+
+  downloadImage() {
+    this.href = document.getElementsByTagName('img')[0].src;
+  }
+
+  createNewAccount() {
+    this.generateMnemonic();
+    this.generateKeyPairs();
+    console.log(this.checked);
+    this.display = false;
+    if (this.checked) {
+      this.sendQrCodeToEmail();
+    }
+  }
 }
