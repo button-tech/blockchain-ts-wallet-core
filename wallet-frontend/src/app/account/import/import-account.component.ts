c31883c97ca64542bf1fc9664d0ec622ed15148b
prazd <alexokda@list.ru>
2020-01-09 21:01:16 +0300

    update import

--- wallet-frontend/src/app/account/import/import-account.component.ts
+++ wallet-frontend/src/app/account/import/import-account.component.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { HdWallet } from '../../shared/services/hd-wallet/hd-wallet.service';
+import {StorageService} from '../../shared/services/storage/storage.service';
+import {Security} from '../../shared/services/security/security.service';
+import {QrCodeData} from '../../shared/shared.module';
+import {Options, QrCode} from "../../shared/components/qrcode/qrcode.service";
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -18,13 +22,40 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styles: [],
 })
 export class ImportAccountComponent {
+  display = true;
+  href: string;
+  @ViewChild('qrcode', { static: false }) qrcode: ElementRef;
   importFormControl  = new FormGroup({
     mnemonic: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
+  downloadImage() {
+    this.href = document.getElementsByTagName('img')[0].src;
+  }
   get mnemonic() {
     return this.importFormControl.value.mnemonic;
   }
+  get password() {
+    return this.importFormControl.value.password;
+  }
 
-  importMnemonic() {
+  importByMnemonic() {
+    // todo: check mnemonic
+    const s = new StorageService();
+    const cypher = Security.encryptSecret(this.mnemonic, this.password);
+    s.cypherParams = { salt: cypher.salt, iv: cypher.iv };
+    s.storage = { secret: cypher.text, expired: false };
+    const qrData: QrCodeData = {
+      mnemonic: cypher.text,
+      iv: cypher.iv,
+      salt: cypher.salt
+    };
+    const opt: Options = { text: JSON.stringify(qrData) };
+    const qr = new QrCode();
+    qr.render(opt, this.qrcode);
+    this.display = false;
+  }
+
+  importByQR() {
   }
 }
