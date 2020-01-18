36e00054210a501ea8bfc97110bbe2a45ed5da4f
Kirill <krboktv@gmail.com>
2020-01-18 03:01:46 +0300

    add exports

---
+++ src/currencies/index.ts
@@ -0,0 +1,6 @@
+// created from 'create-ts-index'
+
+export * from './ethereumBased'
+export * from './stellar'
+export * from './utxoBased'
+export * from './waves'
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// created from 'create-ts-index'
+
+export * from './currencies'
+export * from './DomainCurrency'
+export * from './blockchain.utils'
+export * from './ts-wallet-core'
+export * from './types'
