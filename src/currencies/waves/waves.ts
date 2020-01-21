c32cf006bd5981dfe1f17014dda491b5760e2130
Kirill <krboktv@gmail.com>
2020-01-21 17:42:29 +0300

    Add possibility to run waves via mnemonic phrase without derivation

--- src/currencies/waves/waves.ts
+++ src/currencies/waves/waves.ts
@@ -1,11 +1,10 @@
 import { transfer } from 'waves-transactions';
 import { ITransferTransaction } from 'waves-transactions/transactions';
-import { address } from '@waves/ts-lib-crypto';
+import { address, privateKey, publicKey } from '@waves/ts-lib-crypto';
 import { ICurrency, MnemonicDescriptor, WavesDecimals, WavesTransactionParams } from '../../types';
 import { FromDecimal } from '../../blockchain.utils';
 import { getWavesKeyPair } from '../../hd-wallet';
 import { Buffer } from 'buffer';
-import { box } from 'tweetnacl';
 import * as basex from 'base-x';
 
 export function Waves(secret: string | MnemonicDescriptor): ICurrency {
@@ -16,16 +15,28 @@ export function Waves(secret: string | MnemonicDescriptor): ICurrency {
   return new WavesCurrency(secret);
 }
 
+interface IPrivateKey {
+  privateKey: string;
+}
+
 export class WavesCurrency implements ICurrency {
+  private readonly secret: IPrivateKey;
   private readonly address: string;
 
-  constructor(private readonly privateKey: string) {
-    const bufferKey = new Buffer(privateKey, 'hex');
-    const publicKey = box.keyPair.fromSecretKey(new Uint8Array(bufferKey)).publicKey;
-    const base58PubKey = basex('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz').encode(
-      new Buffer(publicKey)
-    );
-    this.address = address({ publicKey: base58PubKey });
+  constructor(secret: string) {
+    let privK: string;
+
+    if (secret.split(' ').length > 2) {
+      privK = privateKey(secret);
+    } else {
+      privK = basex('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz').encode(
+        new Buffer(secret, 'hex')
+      );
+    }
+
+    const pubK = publicKey({ privateKey: privK });
+    this.address = address({ publicKey: pubK });
+    this.secret = { privateKey: privK };
   }
 
   getAddress(): string {
@@ -35,12 +46,13 @@ export class WavesCurrency implements ICurrency {
   signTransaction(params: WavesTransactionParams): Promise<string> {
     const timestamp = Date.now();
     const signedTx: ITransferTransaction = transfer(
+      // @ts-ignore
       {
         amount: FromDecimal(params.amount, WavesDecimals).toNumber(),
         recipient: params.toAddress,
         timestamp
       },
-      this.privateKey
+      this.secret
     );
 
     return Promise.resolve(JSON.stringify(signedTx));
