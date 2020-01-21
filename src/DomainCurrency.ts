bb7ea28b51fb7617891725edc979132175e8d82e
Kirill <krboktv@gmail.com>
2020-01-21 12:53:44 +0300

    minor fix

--- src/DomainCurrency.ts
+++ src/DomainCurrency.ts
@@ -1,10 +1,10 @@
 export interface IDomainCurrency {
-  full: string
+  full: fullName;
 
-  short: shortName
+  short: shortName;
 }
 
-export type shortName = 'ltc' | 'btc' | 'eth' | 'etc' | 'bch' | 'waves' | 'xlm' | 'ton'
+export type shortName = 'ltc' | 'btc' | 'eth' | 'etc' | 'bch' | 'waves' | 'xlm' | 'ton';
 export type fullName =
   | 'litecoin'
   | 'bitcoin'
@@ -13,116 +13,116 @@ export type fullName =
   | 'bitcoinCash'
   | 'waves'
   | 'stellar'
-  | 'ton'
+  | 'ton';
 
 export class DomainEthereum implements IDomainCurrency {
   static Instance(): DomainEthereum {
-    return new DomainEthereum()
+    return new DomainEthereum();
   }
 
   get full(): fullName {
-    return 'ethereum'
+    return 'ethereum';
   }
 
   get short(): shortName {
-    return 'eth'
+    return 'eth';
   }
 }
 
 export class DomainEthereumClassic implements IDomainCurrency {
   static Instance(): DomainEthereumClassic {
-    return new DomainEthereumClassic()
+    return new DomainEthereumClassic();
   }
 
   get full(): fullName {
-    return 'ethereumClassic'
+    return 'ethereumClassic';
   }
 
   get short(): shortName {
-    return 'etc'
+    return 'etc';
   }
 }
 
 export class DomainBitcoin implements IDomainCurrency {
   static Instance(): DomainBitcoin {
-    return new DomainBitcoin()
+    return new DomainBitcoin();
   }
 
   get full(): fullName {
-    return 'bitcoin'
+    return 'bitcoin';
   }
 
   get short(): shortName {
-    return 'btc'
+    return 'btc';
   }
 }
 
 export class DomainBitcoinCash implements IDomainCurrency {
   static Instance(): DomainBitcoinCash {
-    return new DomainBitcoinCash()
+    return new DomainBitcoinCash();
   }
 
   get full(): fullName {
-    return 'bitcoinCash'
+    return 'bitcoinCash';
   }
 
   get short(): shortName {
-    return 'bch'
+    return 'bch';
   }
 }
 
 export class DomainLitecoin implements IDomainCurrency {
   static Instance(): DomainLitecoin {
-    return new DomainLitecoin()
+    return new DomainLitecoin();
   }
 
   get full(): fullName {
-    return 'litecoin'
+    return 'litecoin';
   }
 
   get short(): shortName {
-    return 'ltc'
+    return 'ltc';
   }
 }
 
 export class DomainWaves implements IDomainCurrency {
   static Instance(): DomainWaves {
-    return new DomainWaves()
+    return new DomainWaves();
   }
 
   get full(): fullName {
-    return 'waves'
+    return 'waves';
   }
 
   get short(): shortName {
-    return 'waves'
+    return 'waves';
   }
 }
 
 export class DomainStellar implements IDomainCurrency {
   static Instance(): DomainStellar {
-    return new DomainStellar()
+    return new DomainStellar();
   }
 
   get full(): fullName {
-    return 'stellar'
+    return 'stellar';
   }
 
   get short(): shortName {
-    return 'xlm'
+    return 'xlm';
   }
 }
 
 export class DomainTON implements IDomainCurrency {
   static Instance(): DomainTON {
-    return new DomainTON()
+    return new DomainTON();
   }
 
   get full(): fullName {
-    return 'ton'
+    return 'ton';
   }
 
   get short(): shortName {
-    return 'ton'
+    return 'ton';
   }
 }
